549d09fdb1292f695d64fdad180a1ff58aea958a
mustafakhan777 <mustafa.khan2214@gmail.com>
2022-11-02 17:11:44 -0500

    done

---
+++ app.js
@@ -0,0 +1,77 @@
+alert ("Thanks For Your Input")
+var name= "Mark";
+name="Ace";
+console.log(name)
+
+var nationality;
+nationality="U.S"
+console.log(nationality)
+
+var floogle="Mark";
+console.log(floogle)
+
+var thanx = "Thanks for your input!"
+alert(thanx);
+
+var weight = 150;
+weight = weight + 25;
+console.log(weight)
+
+var originalNum = 23;
+var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+console.log(newNum)
+
+var originalNum = 90;
+originalNum = originalNum + 10;
+console.log(originalNum)
+
+alert('144')
+var caseQty = 144;
+alert(caseQty);
+
+var popularNumber = 2 + 2;
+var popularNumber = 12 - 24;
+console.log(popularNumber);
+
+var num = 10;
+var anotherNum = 1;
+var popularNumber = num + anotherNum;
+console.log(popularNumber);
+
+// Post Increment
+var num = 1;
+num++
+console.log(num);
+
+// Post Decrement
+var num = 1;
+num--
+console.log(num);
+
+// Pre Increment
+var num = 1;
+++num
+console.log(num);
+
+// Pre Decrement
+var num = 1;
+--num
+console.log(num);
+
+var totalCost = 1 + (3 * 4);
+console.log(totalCost);
+
+var totalCost = (1 + 3) * 4;
+console.log(totalCost);
+
+var a ="10";
+var b = 10;
+var c = a+b;
+console.log(c);
+
+var a ="10";
+var b = 10;
+var c = a*b;
+console.log(c);
+
